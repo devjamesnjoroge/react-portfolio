6d304954beb65f6bc73cea568149fc98caca4cb4
James Ng'ang'a <james.ng'ang'a@student.moringaschool.com>
2022-09-18 21:44:01 +0300

    ft: introduce useState to increment rating

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './Feedback.scss'
 
 function Feedback() {
+    const [rating, setRating] = useState(1)
+
+    const processClick = () => {
+        setRating(
+            (prev)=> {
+                return prev + 1;
+            }
+            )
+    }
+
   return (
     <>
 
     <h2 className='primary'>Feedback app</h2>
+    <div className="circle">
+        <span>{rating}</span>
+    </div>
+    <button className='btn' onClick={processClick}>Update Rating</button>
 
     </>
   )
