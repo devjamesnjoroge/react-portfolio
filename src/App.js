b31e0300af89710c8c519d99ed93139d66931726
James Ng'ang'a <james.ng'ang'a@student.moringaschool.com>
2022-09-22 17:25:05 +0300

    ft-remake: create Navbar component

--- src/App.js
+++ src/App.js
@@ -1,42 +1,9 @@
-import Header from "./components/Header/Header"
-import Hero from "./components/Hero/Hero"
-import Preloader from "./components/Preloader/Preloader"
-import FeedbackData from "./Data/FeedbackData"
-import FeedbackItem from "./components/FeedbackItem/FeedbackItem"
-import PropTypes from 'prop-types'
-import {useState} from 'react'
-import FeedbackStats from "./components/FeedbackStats"
-import FeedbackForm from "./components/Forms/FeedbackForm"
+import Navbar from "./Components/Navbar"
 
-
-function App(){
-    const [feedback, setFeedback] = useState(FeedbackData)
-
-    const handleFeedback = (id) => {
-        if (window.confirm('Do you really want to delete?')){
-            setFeedback(feedback.filter((item) => item.id !== id))
-        }
-    }
-
-    const feedLength = feedback.length
-
-    const addFeedback = () => {
-        alert('Complete add?')
-        setFeedback((prev) =>{
-            return prev.push({id: feedLength + 1, rating: 6, text: 'fghj'})
-        })
-    }
-
-    return (
-        <>
-        <Header />
-        <Hero />
-        <FeedbackForm handleFeed={addFeedback} />
-        <FeedbackStats feedback={feedback} />
-        <FeedbackItem items={feedback} handleDelete={handleFeedback} />
-        <Preloader />
-        </>
-        )
+function App() {
+  return (
+    <Navbar />
+  )
 }
 
 export default App
