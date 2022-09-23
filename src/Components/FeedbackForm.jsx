44e4e1f3f9cdce78dbf5b23b9957cfb208da66f5
James Ng'ang'a <james.ng'ang'a@student.moringaschool.com>
2022-09-23 16:39:15 +0300

    ft: add rating radio buttons

--- src/Components/FeedbackForm.jsx
+++ src/Components/FeedbackForm.jsx
@@ -13,6 +13,48 @@ function FeedbackForm() {
             <label htmlFor="review">
                 Write a review about working with James before. And also leave a rating!
             </label>
+            <ul className="radio-btns">
+                <li>
+                    <label htmlFor="num1">1</label>
+                    <input type="radio" name="rating" id="num1" />
+                </li>
+                <li>
+                    <label htmlFor="num2">2</label>
+                    <input type="radio" name="rating" id="num2" />
+                </li>
+                <li>
+                    <label htmlFor="num3">3</label>
+                    <input type="radio" name="rating" id="num3" />
+                </li>
+                <li>
+                    <label htmlFor="num4">4</label>
+                    <input type="radio" name="rating" id="num4" />
+                </li>
+                <li>
+                    <label htmlFor="num5">5</label>
+                    <input type="radio" name="rating" id="num5" />
+                </li>
+                <li>
+                    <label htmlFor="num6">6</label>
+                    <input type="radio" name="rating" id="num6" />
+                </li>
+                <li>
+                    <label htmlFor="num7">7</label>
+                    <input type="radio" name="rating" id="num7" />
+                </li>
+                <li>
+                    <label htmlFor="num8">8</label>
+                    <input type="radio" name="rating" id="num8" />
+                </li>
+                <li>
+                    <label htmlFor="num9">9</label>
+                    <input type="radio" name="rating" id="num9" />
+                </li>
+                <li>
+                    <label htmlFor="num10">10</label>
+                    <input type="radio" name="rating" id="num10" />
+                </li>
+            </ul>
             <textarea className='review-input' type="text" name="review" id="review"></textarea>
             <Button onClick={handleSubmit}>
                 Send 
