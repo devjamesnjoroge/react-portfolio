83046016af438ffb6d6c5895fbe273fc84c566f5
James Ng'ang'a <james.ng'ang'a@student.moringaschool.com>
2022-09-24 16:09:05 +0300

    ft: deploy to netlify

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import FeedbackForm from "./Components/FeedbackForm"
 import FeedbackList from "./Components/FeedbackList"
 import Navbar from "./Components/Navbar"
-import FeedbackData from "./data/FeedbackData"
-import {useState} from 'react'
 import FeedbackStats from "./Components/FeedbackStats"
 import { FeedbackProvider } from './context/FeedbackContext'
 
 function App() {
-
-  const [feedback, setFeedback] = useState(FeedbackData)
 
   return (
     <FeedbackProvider>
