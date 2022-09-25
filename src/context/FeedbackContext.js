892640665011b76d75f1e9fd58761f7c23aa1fd9
James Ng'ang'a <james.ng'ang'a@student.moringaschool.com>
2022-09-25 11:17:53 +0300

    ft: use fetch api to get data from json server

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,14 +1,29 @@
-import {createContext, useState} from 'react'
-import FeedbackData from '../data/FeedbackData'
+import {createContext, useState, useEffect} from 'react'
 import { v4 as uuidv4 } from 'uuid'
 
 const FeedbackContext = createContext()
 
 export const FeedbackProvider = ({children}) => {
 
-    const [feedback, setFeedback] = useState(FeedbackData)
+    
+
+    const [feedback, setFeedback] = useState([])
 
     const [feedbackEdit, setFeedbackEdit] = useState({item: {}, edit : false})
+
+    // LOAD DATA ON PAGE LOAD
+
+    useEffect(() => {
+      fetchData()
+    }, [])
+
+    // FETCH JSON SERVER DATA
+
+    const fetchData = async() => {
+      const response = await fetch(`http://localhost:5000/feedback?_sort=id&_order=desc`)
+      const data = await response.json()
+      setFeedback(data)
+    }
 
     // Add Feedback
 
